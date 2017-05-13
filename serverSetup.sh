a6d08f15bd81c36d64a212598a3e0a680b1d0a8d
alexbondor <alexandru.viorel.bondor@gmail.com>
2017-05-13 16:54:52 +0300

    Moved utilities and fixed typo

--- serverSetup.sh
+++ serverSetup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 ##
+# Utility methods
+##
+infoMessage() {
+    echo $INFO_MESSAGE $1
+}
+
+successMessage() {
+    echo $SUCCESS_MESSAGE $1
+}
+
+##
 # Global variables
 ##
 INFO_MESSAGE="[INFO] server-setup: "
@@ -49,7 +60,7 @@ fi
 ##
 # Increase swap memory
 ##
-if [ ! -f SWAP ]; then
+if [ ! -f $SWAP ]; then
     infoMessage "Increasing swap memory.."
     swapoff /var/swap
     rm /var/swap
@@ -248,14 +259,3 @@ if [! -f $YOUTRACK ]; then
 
     touch $YOUTRACK
 fi
-
-##
-# Utility methods
-##
-infoMessage() {
-    echo $INFO_MESSAGE $1
-}
-
-successMessage() {
-    echo $SUCCESS_MESSAGE $1
-}
