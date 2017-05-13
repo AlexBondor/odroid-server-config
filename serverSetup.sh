3f347d45c7d97df514019b0f3b999a127882486e
alexbondor <alexandru.viorel.bondor@gmail.com>
2017-05-13 13:31:33 +0300

    Added info and success messages to network, temp and git

--- serverSetup.sh
+++ serverSetup.sh
@@ -8,36 +8,57 @@ TEMP_DIR="/home/server-setup-temp"
 
 CONFIG_REPO_NAME="odroid-server-config"
 CONFIG_REPO_URL="https://github.com/AlexBondor/odroid-server-config.git"
+GIT_EMAIL="alexandru.viorel.bondor@gmail.com"
+GIT_NAME="Alex Bondor"
 
 # configure network
-NAME=eth0
-IP=192.168.1.200
-MASK=255.255.255.0
-GATEWAY=192.168.1.1
-DNS=8.8.8.8
+NAME="eth0"
+IP="192.168.1.200"
+MASK="255.255.255.0"
+GATEWAY="192.168.1.1"
+DNS="8.8.8.8"
 
+infoMessage "Configuring network.. "
+infoMessage "Interface name: " $NAME
+infoMessage "IP: " $IP
+infoMessage "Mask: " $MASK
+infoMessage "Gateway: " $GATEWAY
+infoMessage "DNS: " $DNS
 ifconfig $NAME down;
-ifconfig $NAME $IP $MASK;
+ifconfig $NAME $IP netmask $MASK;
 ifconfig $NAME up
 route add default gw $GATEWAY;
 echo nameserver $DNS > /etc/resolv.conf; 
-
-# create temp directory
-mkdir $TEMP_DIR
-cd $TEMP_DIR
+successMessage "Network configured"
 
 # update repos
+infoMessage "Update reposs.."
 apt-get update
+successMessage "Repos updated"
 
 # enable ssh
 
 # install git
-echo $INFO_MESSAGE "Installing git.."
+infoMessage "Installing git.."
 apt-get install --yes git
-echo $SUCCESS_MESSAGE "git installed"
+successMessage "Git installed"
+
+infoMessage "Configuring git.."
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_NAME
+successMessage "Git configured"
+
+
+# create temp directory
+infoMessage "Create temp directory " $TEMP_DIR ".."
+mkdir $TEMP_DIR
+cd $TEMP_DIR
+successMessage "Temp directory created and moved into"
 
 # clone config repository
+infoMessage "Cloning config repo from " $CONFIG_REPO_URL
 git clone $CONFIG_REPO_URL
+successMessage "Config repo successfully cloned"
 
 # install mysql
 
@@ -46,3 +67,11 @@ git clone $CONFIG_REPO_URL
 # install teamcity
 
 # install youtrack
+
+infoMessage() {
+    echo $INFO_MESSAGE $1
+}
+
+successMessage() {
+    echo $SUCCESS_MESSAGE $1
+}
